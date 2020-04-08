5c3f6584d460c3ddd5c439479892f61ed713197c
Patrick Nyatindo <nyatindopatrick@gmail.com>
2020-04-08 12:17:02 +0300

    Tweet class and create @twits variable

--- lib/tweet_controller.rb
+++ lib/tweet_controller.rb
@@ -10,18 +10,19 @@ class Tweet
       config.access_token = ENV['ACCESS_TOKEN']
       config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
     end
+    @twits = []
   end
 
   def fetch_tweets
-    @client.search('#javascript', result_type: 'recent').take(2)
+    @twits = @client.search('#javascript', result_type: 'recent').take(2)
   end
 
   def like_tweets
-    @client.favorite(fetch_tweets)
+    @client.favorite(@twits)
   end
 
   def retweets
-    fetch_tweets.each do |t|
+    @twits.each do |t|
       @client.retweet(t.id)
     end
   end
